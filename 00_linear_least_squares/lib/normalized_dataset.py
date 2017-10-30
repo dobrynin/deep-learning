898cd20014181e36cbb609ad4e8dd05134b13e97
Ned Ruggeri <ruggeri@self-loop.com>
2017-10-29 17:00:33 -0700

    writeup how to undo the normalized parameters

--- 00_linear_least_squares/lib/normalized_dataset.py
+++ 00_linear_least_squares/lib/normalized_dataset.py
@@ -6,9 +6,11 @@ class NormalizedDataset(Dataset):
         self.x_mean = np.mean(dataset.x)
         self.x_stddev = np.sqrt(np.var(dataset.x))
         self.x = (dataset.x - self.x_mean) / self.x_stddev
+        self.unnormalized_x = dataset.x
 
         self.y_mean = np.mean(dataset.y)
         self.y_stddev = np.sqrt(np.var(dataset.y))
         self.y = (dataset.y - self.y_mean) / self.y_stddev
+        self.unnormalized_y = dataset.y
 
 NORMALIZED_DATASET = NormalizedDataset(DATASET)
