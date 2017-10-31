3ab7572d61689e4e6e4ef6e45db9dcaf4467c989
Ned Ruggeri <ruggeri@self-loop.com>
2017-10-31 11:06:48 -0700

    do training/test split

--- 01_logistic_regression/lib/dataset.py
+++ 01_logistic_regression/lib/dataset.py
@@ -1,16 +1,42 @@
 from lib.config import DATA_DIR
 import os.path
 import pickle
+import zlib
 
-class RawDataset:
-    INSTANCE = None
-
+class Dataset:
     def __init__(
             self, word_encoding_dictionary, ham_emails, spam_emails
     ):
         self.word_encoding_dictionary = word_encoding_dictionary
         self.ham_emails = ham_emails
         self.spam_emails = spam_emails
+
+    def split(self, ratio):
+        datasetA = self._split(ratio, 0)
+        datasetB = self._split(ratio, 1)
+        return (datasetA, datasetB)
+
+    def _split(self, ratio, mode):
+        split_ham_emails, split_spam_emails = [], []
+        emails_pairs = [
+            (self.ham_emails, split_ham_emails),
+            (self.spam_emails, split_spam_emails)
+        ]
+        for (emails, split_emails) in emails_pairs:
+            for email in emails:
+                h = zlib.crc32(email.path.encode())
+                p = h / (2**32 - 1)
+                if (mode == 0 and p < ratio) or (mode == 1 and p >= ratio):
+                    split_emails.append(email)
+
+        return Dataset(
+            self.word_encoding_dictionary,
+            ham_emails = split_ham_emails,
+            spam_emails = split_spam_emails
+        )
+
+class RawDataset(Dataset):
+    INSTANCE = None
 
     @classmethod
     def get(cls):
