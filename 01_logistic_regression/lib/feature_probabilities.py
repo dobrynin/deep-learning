try to fix stupidity around improper feature prob ratio calculation

01_logistic_regression/lib/feature_probabilities.py
@@ -1,4 +1,4 @@
-import math
+import numpy as np
 
 class Counts:
     def __init__(self, ham_count = 0, spam_count = 0):
@@ -9,39 +9,24 @@ class Counts:
     def total_count(self):
         return self.ham_count + self.spam_count
 
-    def to_probs(self):
-        return Probs(self)
-
-    def to_odds(self):
-        return self.to_probs().to_odds()
-
     def __repr__(self):
         return self.__dict__.__repr__()
 
-class Probs:
-    def __init__(self, counts):
-        self.ham_prob, self.spam_prob = (
-            counts.ham_count / counts.total_count(),
-            counts.spam_count / counts.total_count()
+class ConditionalFeatureProbabilityRatio:
+    def __init__(self, feature_counts, class_counts):
+        prob_feature_given_ham = (
+            feature_counts.ham_count / class_counts.ham_count
+        )
+        prob_feature_given_spam = (
+            feature_counts.spam_count / class_counts.spam_count
         )
 
-    def to_odds(self):
-        return Odds(self)
-
-    def __repr__(self):
-        return self.__dict__.__repr__()
-
-class Odds:
-    def __init__(self, probs):
-        if probs.spam_prob != 0.0:
-            self.ham_odds = probs.ham_prob / probs.spam_prob
+        if (prob_feature_given_ham != 0):
+            self.feature_probability_ratio = (
+                prob_feature_given_spam / prob_feature_given_ham
+            )
         else:
-            self.ham_odds = math.inf
-
-        if probs.ham_prob != 0.0:
-            self.spam_odds = probs.spam_prob / probs.ham_prob
-        else:
-            self.spam_odds = math.inf
+            self.feature_probability_ratio = np.inf
 
     def __repr__(self):
         return self.__dict__.__repr__()
@@ -76,8 +61,11 @@ class FeatureProbabilities:
     def class_probs(self):
         return self.class_counts.to_probs()
 
-    def code_given_class_prob(self, code):
-        return self.code_counts[code].to_probs()
+    def code_prob_ratio(self, code):
+        return ConditionalFeatureProbabilityRatio(
+            feature_counts = self.code_counts[code],
+            class_counts = self.class_counts
+        )
 
     def filter(self, reach_limit):
         filtered_fps = (type(self))()
@@ -88,7 +76,7 @@ class FeatureProbabilities:
 
         return filtered_fps
 
-    def no_code_given_class_prob(self, code):
+    def no_code_prob_ratio(self, code):
         code_counts = self.code_counts[code]
         no_code_counts = Counts(
             ham_count = (
@@ -99,7 +87,10 @@ class FeatureProbabilities:
             )
         )
 
-        return no_code_counts.to_probs()
+        return ConditionalFeatureProbabilityRatio(
+            feature_counts = no_code_counts,
+            class_counts = self.class_counts
+        )
 
     def _check_code_added(self, code):
         if code in self.code_counts: return

01_logistic_regression/lib/naive_bayes_model.py
@@ -1,4 +1,6 @@
-class Result:
+import numpy as np
+
+class RecallResult:
     def __init__(self, score_cutoff, num_spams_identified, recall):
         self.score_cutoff, self.num_spams_identified, self.recall = (
             score_cutoff, num_spams_identified, recall
@@ -14,11 +16,11 @@ class NaiveBayesModel:
         for code in self.fps.code_counts:
             if code in email.codes:
                 spam_score *= (
-                    self.fps.code_given_class_prob(code).to_odds().spam_odds
+                    self.fps.code_prob_ratio(code).feature_probability_ratio
                 )
             elif self.use_negative_features:
                 spam_score *= (
-                    self.fps.no_code_given_class_prob(code).to_odds().spam_odds
+                    self.fps.no_code_prob_ratio(code).feature_probability_ratio
                 )
 
         return spam_score
@@ -44,12 +46,49 @@ class NaiveBayesModel:
                 num_spams_identified / len(dataset.spam_emails)
             )
 
-            return Result(
+            return RecallResult(
                 score_cutoff = score_cutoff,
                 num_spams_identified = num_spams_identified,
                 recall = recall,
             )
 
+        return [
+            (limit, calculate_result(limit)) for limit in limits
+        ]
+
+    def spam_probability_at_different_scores(self, dataset):
+        ham_scores = list(map(
+            lambda email: self.score_email(email),
+            dataset.ham_emails
+        ))
+        spam_scores = list(map(
+            lambda email: self.score_email(email),
+            dataset.spam_emails
+        ))
+
+        def calculate_result(limit):
+            score_cutoff = limit / (1 - limit)
+            print(score_cutoff)
+            num_true_positives = sum(
+                [1 if s > score_cutoff else 0 for s in spam_scores]
+            )
+            num_false_positives = sum(
+                [1 if s > score_cutoff else 0 for s in ham_scores]
+            )
+            num_positives = num_true_positives + num_false_positives
+            if num_positives == 0:
+                return None
+            num_expected_positives = sum(
+                [(s / (s + 1)) if s > score_cutoff else 0 for s in spam_scores]
+            )
+
+            return {
+                "limit": limit,
+                "expected_rate": num_expected_positives / num_positives,
+                "true_rate": num_true_positives / num_positives
+            }
+
+        limits = np.arange(0.5, 1.0, 0.1)
         return [
             (limit, calculate_result(limit)) for limit in limits
         ]