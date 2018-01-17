093307e93619607fcf7d6af9acf7bdbe46bb3f07
David Dobrynin <david.dobrynin@gmail.com>
2018-01-17 04:25:08 +0000

    Fix bugs

--- tutoring/style/network.py
+++ tutoring/style/network.py
@@ -6,7 +6,8 @@ from util import save_image, open_image
 from keras.models import Model
 from keras.layers import Input, Dense, Lambda, Reshape
 import keras.backend as K
-from keras.optimizers import SGD
+from keras.optimizers import Adam
+from keras.callbacks import LambdaCallback
 
 def build_style_tensor(content_tensor):
     height = content_tensor.shape[1].value
@@ -29,29 +30,30 @@ for layer in vgg_model.layers:
     print(layer)
     layer.trainable = False
 
-block1_conv2 = vgg_model.get_layer('block1_conv2')
-block2_conv2 = vgg_model.get_layer('block2_conv2')
-block3_conv2 = vgg_model.get_layer('block3_conv2')
-block4_conv2 = vgg_model.get_layer('block4_conv2')
-block5_conv2 = vgg_model.get_layer('block5_conv2')
+block1_conv1 = vgg_model.get_layer('block1_conv1')
+block2_conv1 = vgg_model.get_layer('block2_conv1')
+block3_conv1 = vgg_model.get_layer('block3_conv1')
+block4_conv1 = vgg_model.get_layer('block4_conv1')
+block5_conv1 = vgg_model.get_layer('block5_conv1')
 
-content_output_tensor = block5_conv2.output
+block4_conv2 = vgg_model.get_layer('block4_conv2')
+content_output_tensor = block4_conv2.output
 
 style_layer = Lambda(build_style_tensor)
-block1_conv2_style_tensor = style_layer(block1_conv2.output)
-block2_conv2_style_tensor = style_layer(block2_conv2.output)
-block3_conv2_style_tensor = style_layer(block3_conv2.output)
-block4_conv2_style_tensor = style_layer(block4_conv2.output)
-block5_conv2_style_tensor = style_layer(block5_conv2.output)
+block1_conv1_style_tensor = style_layer(block1_conv1.output)
+block2_conv1_style_tensor = style_layer(block2_conv1.output)
+block3_conv1_style_tensor = style_layer(block3_conv1.output)
+block4_conv1_style_tensor = style_layer(block4_conv1.output)
+block5_conv1_style_tensor = style_layer(block5_conv1.output)
 
 
 model = Model(input=vgg_model.input, outputs=[
     content_output_tensor,
-    block1_conv2_style_tensor,
-    block2_conv2_style_tensor,
-    block3_conv2_style_tensor,
-    block4_conv2_style_tensor,
-    block5_conv2_style_tensor])
+    block1_conv1_style_tensor,
+    block2_conv1_style_tensor,
+    block3_conv1_style_tensor,
+    block4_conv1_style_tensor,
+    block5_conv1_style_tensor])
 
 content_matrix, *_ = model.predict(content_image_array)
 _, *style_matrices = model.predict(style_image_array)
@@ -68,13 +70,21 @@ styled_image_model = Model(input=input_tensor, outputs=[
     *final_styles
 ])
 
-sgd = SGD(lr=100)
-styled_image_model.compile(optimizer=sgd, loss='mse')
+adam = Adam(lr=10)
+styled_image_model.compile(optimizer=adam, loss='mse',loss_weights=[1, *([20]*5)])
 styled_image_model.summary()
+
+def save_image_callback(epoch, logs):
+    if epoch % 100 == 0:
+        weights = dense_layer.get_weights()[0]
+        reshaped_weights = np.reshape(weights, (768, 1024, 3))
+        save_image(reshaped_weights, f"./images/stylized_image_{epoch}.jpg")
+
+save_image_callback_object = LambdaCallback(
+        on_epoch_end=save_image_callback)
+
 styled_image_model.fit(
-    np.ones((1,1)), [content_matrix, *style_matrices], epochs=10
+    np.ones((1,1)), [content_matrix, *style_matrices], epochs = 10000,
+    callbacks=[save_image_callback_object]
 )
 
-weights = dense_layer.get_weights()[0]
-reshaped_weights = np.reshape(weights, (768, 1024, 3))
-save_image(reshaped_weights, "./images/stylized_image.jpg")
--- tutoring/style/util.py
+++ tutoring/style/util.py
@@ -1,21 +1,32 @@
 from PIL import Image
 import numpy as np
+
+_R_MEAN = 123.68
+_G_MEAN = 116.78
+_B_MEAN = 103.94
+
 def open_image(image_path):
     image = Image.open(image_path)
-    image_array = np.array(image)
+    image_array = np.array(image, dtype=np.float32)
     reds = np.copy(image_array[:, :, 0])
     blues = image_array[:, :, 2]
     image_array[:, :, 0] = blues
     image_array[:, :, 2] = reds
+    image_array[:, :, 0] -= _B_MEAN
+    image_array[:, :, 1] -= _G_MEAN
+    image_array[:, :, 2] -= _R_MEAN
     return image_array
 
 def save_image(image_data, image_path):
     image_data = np.copy(image_data)
+    image_data[:, :, 0] += _B_MEAN
+    image_data[:, :, 1] += _G_MEAN
+    image_data[:, :, 2] += _R_MEAN
     reds = np.copy(image_data[:, :, 0])
     blues = image_data[:, :, 2]
     image_data[:, :, 0] = blues
     image_data[:, :, 2] = reds
-    image = Image.fromarray(np.uint8(image_data))
+    image = Image.fromarray(np.uint8(np.clip(image_data, 0, 255)))
     image.save(image_path)
 
 image_array = open_image('./images/Tuebingen.jpg')
